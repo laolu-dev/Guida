e1a19c83eec4c2d27048842401c462ba5d14d8f2
= <=>
2025-10-10 18:04:26 +0100

    chore: Update iOS project configuration and dependencies

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Flutter
-import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,8 +7,6 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    var googleMapApiKey: String = Bundle.main.infoDictionary?["GoogleMapApiKey"] as? String ?? ""
-    GMSServices.provideAPIKey(googleMapApiKey)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
